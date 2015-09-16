8fa2c5c68e39fc4c655cbafaa1bd41c47924ab52
Horvi, Johnny <Johnny.Horvi@nav.no>
2015-09-16 15:15:20 +0200

    changed ports to standard

--- api/config/config.js
+++ api/config/config.js
@@ -3,7 +3,7 @@ var rootPath = path.normalize(__dirname + '/..')
 
 var config = {
   root: rootPath,
-  port: process.env['PORT'] || 6969,
+  port: process.env['PORT'] || 8080,
   dbUrl: process.env['seraDb_url'] || 'mongodb://localhost:27017/sera',
   dbUser: process.env['seraDb_username'] || 'sera',
   dbPassword: process.env['seraDb_password'] || 'sera'
