08d85b3e004ffdbbdbb8769d850f74f13f11abc1
Horvi, Johnny <Johnny.Horvi@nav.no>
2015-09-25 14:14:17 +0200

    la til status

--- api/models/server.js
+++ api/models/server.js
@@ -1,62 +1,66 @@
 module.exports = {
-  hostname: {
-    type: String,
-    schemaType: 'string'
-  },
-  ipAddress: {
-    type: String,
-    schemaType: 'string'
-  },
-  environment: {
-    type: String,
-    schemaType: 'string'
-  },
-  environmentClass: {
-    type: String,
-    schemaType: {'enum': ['u', 't', 'q', 'p']}
-  },
-  application: {
-    type: String,
-    schemaType: 'string'
-  },
-  type: {
-    type: String,
-    schemaType: 'string'
-  },
-  owner: {
-    type: String,
-    schemaType: 'string'
-  },
-  cpu: {
-    type: Number,
-    schemaType: 'number'
-  },
-  disk: {
-    type: Number,
-    schemaType: 'number'
-  },
-  memory: {
-    type: Number,
-    schemaType: 'number'
-  },
-  notes: {
-    type: String,
-    schemaType: 'string'
-  },
-  os: {
-    type: String,
-    schemaType: {'enum': ['windows', 'rhel']}
-  },
-  site: {
-    type: String,
-    schemaType: {'enum': ['so8', 'u89']}
-  },
-  custom: {
-    type: Boolean,
-    schemaType: 'boolean'
-  },
-  srm: {
-    type: Boolean,
-    schemaType: 'boolean'
-  }
+    hostname: {
+        type: String,
+        schemaType: 'string'
+    },
+    ipAddress: {
+        type: String,
+        schemaType: 'string'
+    },
+    environment: {
+        type: String,
+        schemaType: 'string'
+    },
+    environmentClass: {
+        type: String,
+        schemaType: {'enum': ['u', 't', 'q', 'p']}
+    },
+    status: {
+        type: String,
+        schemaType: {'enum': ['poweredOn', 'poweredOff', 'suspended']}
+    },
+    application: {
+        type: String,
+        schemaType: 'string'
+    },
+    type: {
+        type: String,
+        schemaType: 'string'
+    },
+    owner: {
+        type: String,
+        schemaType: 'string'
+    },
+    cpu: {
+        type: Number,
+        schemaType: 'number'
+    },
+    disk: {
+        type: Number,
+        schemaType: 'number'
+    },
+    memory: {
+        type: Number,
+        schemaType: 'number'
+    },
+    notes: {
+        type: String,
+        schemaType: 'string'
+    },
+    os: {
+        type: String,
+        schemaType: {'enum': ['windows', 'rhel']}
+    },
+    site: {
+        type: String,
+        schemaType: {'enum': ['so8', 'u89']}
+    },
+    custom: {
+        type: Boolean,
+        schemaType: 'boolean'
+    },
+    srm: {
+        type: Boolean,
+        schemaType: 'boolean'
+    }
 }
