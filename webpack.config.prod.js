9cf818ad6871f94fbc11a11a572434107a779b1e
h141513 <even.haasted@nav.no>
2017-04-11 11:00:21 +0200

    tilpasset fra fasit til sera

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,7 +9,7 @@ module.exports = {
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'fasit.js',
+        filename: 'sera.js',
         publicPath: '/'
     },
     plugins: [
