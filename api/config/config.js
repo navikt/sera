c9bd03e7991a113a2b45cf5e94458832b9771291
h141513 <even.haasted@nav.no>
2017-04-18 14:09:19 +0200

    test av docker #5

--- api/config/config.js
+++ api/config/config.js
@@ -10,7 +10,8 @@ const config = {
     tlsCert: process.env['TLS_CERT'] || "localhost.crt",
     fasitNodesUrl: process.env['fasit:nodes_v2_url'] || 'https://fasit.adeo.no/api/v2/nodes',
     cocaUrl: process.env['costService_url'] || 'https://coca.adeo.no/api/v1/calculator/',
-    noraUrl: process.env['units_v1_url'] || 'https://nora.adeo.no/api/v1/units',
+    // noraUrl: process.env['units_v1_url'] || 'https://nora.adeo.no/api/v1/units',
+    noraUrl: 'https://nora.adeo.no/api/v1/units',
     influxUrl: process.env['influxdb_url'],
     influxUser: process.env['influxdb_username'],
     influxPassword: process.env['influxdb_password']
