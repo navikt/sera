07a5a8c40ddb76a7a2c974954732a0547df4ac3e
h141513 <even.haasted@nav.no>
2017-04-21 15:14:56 +0200

    Test av tester

--- production_server.js
+++ production_server.js
@@ -44,22 +44,20 @@ const httpsServer = https.createServer({
     cert: fs.readFileSync(config.tlsCert)
 }, app);
 
-
 if (process.env.NODE_ENV === 'test') {
+    mongoose.connect(config.dbUrlTest)
+    mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
+    httpsServer.listen(8869, function () {
+        logger.info('Running in test environment')
+        logger.info('Connected to MongoDB URL', config.dbUrlTest)
+        logger.info('Listening on port', 8869)
+    })
+} else {
+
     mongoose.connect(config.dbUrl)
     mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
     httpsServer.listen(config.port, function () {
-
-        logger.info('Running in test environment')
-        logger.info('Connected to MongoDB URL', config.dbUrl)
-        logger.info('Listening on port', config.port)
-    })
-}
-
-mongoose.connect(config.dbUrl)
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
-httpsServer.listen(config.port, function () {
-    console.log(`
+        console.log(`
   ______________________________    _____   
  /   _____/\\_   _____/\\______   \\  /  _  \\  
  \\_____  \\  |    __)_  |       _/ /  /_\\  \\ 
@@ -67,10 +65,11 @@ httpsServer.listen(config.port, function () {
 /_______  //_______  / |____|_  /\____|__   /
         \\/         \\/         \\/         \\/ 
 `)
-    logger.info('Running in production environment')
-    logger.info('Connected to MongoDB URL', config.dbUrl)
-    logger.info('Listening on port', config.port)
-})
+        logger.info('Running in production environment')
+        logger.info('Connected to MongoDB URL', config.dbUrl)
+        logger.info('Listening on port', config.port)
+    })
+}
 
 
 module.exports = app
--- test/api/api.js
+++ test/api/api.js
@@ -2,17 +2,10 @@
 const nock = require('nock')
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-// const api = require('../../production_server')
-const api = require('../../api/index')
+const api = require('../../production_server')
+// const api = require('../../api/index')
 const should = chai.should();
-// const mongoose = require('mongoose')
-// const Mockgoose = require('mockgoose').Mockgoose
-// const mockgoose = new Mockgoose(mongoose)
-// const config = require('../../api/config/config')
 
-// mockgoose.prepareStorage().then(function() {
-//     mongoose.connect(config.dbUrl)
-// })
 
 chai.use(chaiHttp);
 
