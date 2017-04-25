2ca0ae81ab429c1ca4ad7dd02715f1bdee17a6a8
h141513 <even.haasted@nav.no>
2017-04-25 11:59:12 +0200

    Endret selftest til å lese appconfig og benytte async modul

--- api/nav-nodeapp-selftest/selftest.js
+++ api/nav-nodeapp-selftest/selftest.js
@@ -1,21 +1,22 @@
 const fs = require('fs')
 const parseString = require('xml2js').parseString
 const request = require('request')
+const async = require('async')
 const TimestampModel = require('../models/timestamp')
 const logger = require('../logger')
 const config = require('../config/config')
 
-let selftestResponse = {
-    application: '',
-    version: '',
-    timestamp: '',
-}
+let selftestResponse = {}
+let checks = []
+let endpoint = []
+let datasource = []
+let baseUrl = []
 
 exports.selftest = function () {
     return function (req, res) {
+        checks = []
         readAppInfo()
     }
-
 }
 
 const readAppInfo = function () {
@@ -24,69 +25,103 @@ const readAppInfo = function () {
             throw err
         }
         parseString(data.toString(), function (err, result) {
-            console.log(result)
+            selftestResponse = {
+                application: result.project.artifactId[0],
+                version: result.project.version[0]
+            }
+            readAppConfig()
+            console.log(selftestResponse)
         })
     })
 }
 
+const readAppConfig = function (testFunc) {
+    fs.readFile('app-config/src/main/resources/app-config.xml', function (err, data) {
+        if (err) {
+            throw err
+        }
+        parseString(data.toString(), function (err, result) {
+            endpoint = result.application.resources[0].rest
+            datasource = result.application.resources[0].datasource
+            baseUrl = result.application.resources[0].baseUrl
 
-
-
-exports.readAppConfig = function () {
-    return function (req, res) {
-        fs.readFile('app-config/src/main/resources/app-config.xml', function (err, data) {
-            if (err) {
-                throw err
-            }
-            parseString(data.toString(), function (err, result) {
-                result.application.resources[0].rest.forEach(function (e, i) {
-                    console.log(e.$.alias)
-                })
-
-
-                console.log(result.application.resources[0].rest.$)
-                res.header('Content-Type', 'application/json; charset=utf-8')
-                res.status(200).send(result.application.resources[0])
-
-            })
+            requestRestEndpoint(endpoint)
         })
-    }
+    })
 }
 
-
-exports.database = function () {
-    return function (request, res, next) {
-        TimestampModel.find({}, function (error, response) {
-            if (error) {
-                res.status(500).send(error)
-            } else {
-                res.header('Content-Type', 'application/json; charset=utf-8')
-                res.json(response)
-                res.status(200).send()
-            }
-        })
-    }
-}
-
-exports.pingRestEndpoint = function () {
-    return function (req, res) {
+const requestRestEndpoint = function (endpoint) {
+    async.each(endpoint, function (currentEndpoint, callback) {
+        const currentRestEndpoint = currentEndpoint + '_url'
+        const url = process.env[currentRestEndpoint] || 'http://navet.adeo.no/' // use dummy URL if running locally
         request.get({
-            url: config.fasitNodesUrl,
+            url: url,
             time: true
-        }, function (error, response, body) {
+        }, function (error, response) {
             if (!error && response.statusCode == 200) {
-                res.header('Content-Type', 'application/json; charset=utf-8')
-                console.log(response.statusCode)
-                console.log(response.elapsedTime)
-                res.status(200).send('Fasit OK!')
+                checks.push({
+                    endpoint: url,
+                    result: 0,
+                    responseTime: response.elapsedTime
+                })
+                callback()
             } else {
-                res.header('Content-Type', 'application/json; charset=utf-8')
-                console.log(error)
-                console.log(response.statusCode)
-                console.log(response.elapsedTime)
-                res.status(200).send('Fasit nede :(')
+                checks.push({
+                    endpoint: url,
+                    result: 1,
+                    errorMessage: response.statusCode,
+                    responseTime: response.elapsedTime
+                })
+                callback()
             }
-
         })
-    }
+    }, function(err) {
+        requestBaseUrl(baseUrl)
+    })
 }
+
+const requestBaseUrl = function (baseUrl) {
+    async.each(baseUrl, function (currentEndpoint, callback) {
+        const currentRestEndpoint = currentEndpoint + '_url'
+        const url = process.env[currentRestEndpoint] || 'http://influxdb.adeo.no:8086' // use dummy URL if running locally
+        request.get({
+            url: url,
+            time: true
+        }, function (error, response) {
+            if (!error && response.statusCode == 200) {
+                checks.push({
+                    endpoint: url,
+                    result: 0,
+                    responseTime: response.elapsedTime
+                })
+                callback()
+            } else {
+                checks.push({
+                    endpoint: url,
+                    result: 1,
+                    errorMessage: response.statusCode,
+                    responseTime: response.elapsedTime
+                })
+                callback()
+            }
+        })
+    }, function(err) {
+        console.log(checks)
+    })
+}
+
+// exports.database = function () {
+//     return function (request, res, next) {
+//         TimestampModel.find({}, function (error, response) {
+//             if (error) {
+//                 res.status(500).send(error)
+//             } else {
+//                 res.header('Content-Type', 'application/json; charset=utf-8')
+//                 res.json(response)
+//                 res.status(200).send()
+//             }
+//         })
+//     }
+// }
+
+
