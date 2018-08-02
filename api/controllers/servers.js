109da9da1d89b94cc1c2393579f55fe574a66bc4
Even <even.haasted@nav.no>
2018-08-02 13:05:27 +0200

    .

--- api/controllers/servers.js
+++ api/controllers/servers.js
@@ -55,57 +55,7 @@ exports.getServers = () => {
             else {
                 logger.info(`Retrieved ${servers[0].items.length} elements from database`)
                 res.status(200).send(servers[0].items)
-                convertToCsv(servers[0].items.toObject())
             }
         })
     }
-}
-
-const convertToCsv = async (items) => {
-    const servers = removeKeys(items, ['_id'])
-    const fields = getFieldsFromArr(servers)
-    const csv = generateCSV(servers, fields)
-}
-
-const getFieldsFromArr = (arr) => {
-    let fields = []
-    arr.forEach((e, i) => {
-        const keys = createFields(e)
-        keys.forEach(element => {
-            if (!fields.includes(element)) fields.push(element)
-        })
-    })
-    return fields
-}
-
-const generateCSV = (items, fields) => {
-    // Create CSV header
-    fields.splice(0, 1)
-    fields.sort()
-    fields.unshift('hostname')
-    let csv = ''
-    fields.forEach((e, i) => {
-        if (i < fields.length - 1) csv += '"' + e + '"' + ','
-        else csv += '"' + e + '"' + '\n'
-    })
-    // Create CSV rows
-    items.forEach(e => {
-        fields.forEach(field => {
-            if (field.match(/([\w]+\.)+[\w]+(?=[\s]|$)/)) {
-                const keys = field.split('.')
-                let objectKey = e
-                keys.forEach(key => {
-                    objectKey = objectKey[key]
-                })
-                if (!objectKey) csv += '"n/a"' + ','
-                else csv += '"' + objectKey + '"' + ','
-            } else {
-                if (!e[field]) csv += '"n/a"' + ','
-                else csv += '"' + e[field] + '"' + ','
-            }
-        })
-        csv = csv.substring(0, csv.length - 1)
-        csv += '\n'
-    })
-    return csv
 }
